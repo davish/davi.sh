96ead1b5de9861d5a4d226305bab1c68c296e20f
Davis Haupt <davis.haupt@gmail.com>
2022-09-18 01:25:27 -0400

    initial commit

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import type { MarkdownLayoutProps } from "astro";
+
 const months = [
   "Jan",
   "Feb",
@@ -14,5 +16,18 @@ const months = [
 ];
 
 export function toReadableString(d: Date) {
-  return `${months[d.getMonth()]} ${d.getDate()} ${d.getFullYear()}`;
+  return `${months[d.getUTCMonth()]} ${d.getUTCDate()} ${d.getUTCFullYear()}`;
+}
+
+export function getSummary<T extends Record<string, any>>(
+  props: MarkdownLayoutProps<T>
+) {
+  const html = props.compiledContent();
+  const moreSplit = html.split("<!--more-->");
+  if (moreSplit.length > 1) {
+    return moreSplit[0];
+  } else {
+    const firstPara = html.indexOf("</p>");
+    return html.slice(0, firstPara + 4);
+  }
 }
