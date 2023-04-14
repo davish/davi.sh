5963e3adeedcbd8ca2bbd81ee32b53991a6050ac
Davis Haupt <davis.haupt@gmail.com>
2023-04-14 17:04:16 -0400

    add tufte-style sidenotes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import { defineConfig } from "astro/config";
+import remarkSidenotes from "@tufte-markdown/remark-sidenotes";
+
+function hasFootnotesFrontmatterPlugin() {
+  return function (tree, file) {
+    let footnotes = tree.children.filter(
+      ({ type }) => type === "footnoteDefinition"
+    );
+    file.data.astro.frontmatter.hasFootnotes = footnotes.length > 0;
+  };
+}
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://davi.sh/",
   markdown: {
+    remarkPlugins: [hasFootnotesFrontmatterPlugin, remarkSidenotes],
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
