8ab91d38cd6a15e8e000b4e388a532a4d4d70c72
Davis Haupt <davis.haupt@gmail.com>
2023-04-23 09:25:14 -0400

    markdoc start

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import remarkSidenotes from "@tufte-markdown/remark-sidenotes";
 
+import markdoc from "@astrojs/markdoc";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://davi.sh/",
@@ -9,7 +11,8 @@ export default defineConfig({
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: "solarized-light",
-    },
+      theme: "solarized-light"
+    }
   },
+  integrations: [markdoc()]
 });
