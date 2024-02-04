6bb8c662f5cf9606f1a0524680e6f759b5ad18fd
Davis Haupt <davis.haupt@gmail.com>
2024-02-04 12:02:55 -0500

    add og-image

---
+++ src/components/og_image.tsx
@@ -0,0 +1,146 @@
+import satori from "satori";
+import fs from "node:fs/promises";
+import { toReadableString } from "src/utils";
+
+// Design inspiration from Jacob Kaplan-Moss: https://jacobian.org
+
+const accentColor = "#0fa817";
+const backgroundColor = "rgba(27, 31, 35, 0.10)";
+
+type ImageProps = {
+  title: string;
+  subtitle: string | null;
+  date: Date;
+  path: string;
+  tags?: string[];
+};
+
+const OGImage = ({ title, subtitle, date, path, tags = [] }: ImageProps) => (
+  <div
+    style={{
+      height: "100%",
+      width: "100%",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#fff",
+      color: "#111",
+      fontSize: 32,
+      fontWeight: 400,
+      background: `linear-gradient(135deg, ${accentColor}, rgba(15, 168, 23, 0.206))`,
+    }}
+  >
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        border: `5px solid #111`,
+        borderRadius: "15px",
+        alignContent: "center",
+        justifyContent: "space-between",
+        justifyItems: "center",
+        width: 1000,
+        height: 500,
+        background: "white",
+        boxShadow: `5px 5px 10px #111`,
+      }}
+    >
+      <div
+        style={{
+          color: "black",
+          fontWeight: 600,
+          display: "flex",
+          alignItems: "center",
+          flex: 1,
+          marginLeft: 30,
+        }}
+      >
+        &gt; ~/{path}
+        <span
+          style={{
+            display: "block",
+            width: 15,
+            height: 28,
+            marginLeft: 5,
+            background: accentColor,
+            opacity: 0.65,
+          }}
+        ></span>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          marginLeft: 50,
+          color: "#111",
+          justifyContent: "space-around",
+        }}
+      >
+        <span style={{ fontSize: 64, fontWeight: 600 }}>{title}</span>
+      </div>
+      <div style={{ flex: 1 }}></div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          width: "100%",
+          justifyContent: "flex-start",
+          marginLeft: 50,
+        }}
+      >
+        Davis Haupt // {toReadableString(date)}
+        {tags.length > 0 ? (
+          <div
+            style={{
+              display: "flex",
+              fontWeight: 400,
+              flex: 0.5,
+            }}
+          >
+            {tags.slice(0, 3).map((tag) => (
+              <span
+                style={{
+                  backgroundColor: backgroundColor,
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  padding: "20px 10px",
+                  height: 24,
+                  fontSize: 24,
+                  borderRadius: 5,
+                  marginRight: 10,
+                  marginBlockStart: 0,
+                }}
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+        ) : null}
+      </div>
+    </div>
+  </div>
+);
+
+export default async (props: ImageProps) =>
+  await satori(<OGImage {...props}></OGImage>, {
+    width: 1200,
+    height: 630,
+    fonts: [
+      {
+        name: "Inter",
+        data: await fs.readFile("./public/fonts/inter-latin-400-normal.ttf"),
+        weight: 400,
+        style: "normal",
+      },
+      {
+        name: "Inter",
+        data: await fs.readFile("./public/fonts/inter-latin-600-normal.ttf"),
+        weight: 600,
+        style: "normal",
+      },
+    ],
+  });
---
+++ src/pages/[...collection]/[...slug]/card.png.ts
@@ -0,0 +1,79 @@
+import sharp from "sharp";
+import type { APIRoute } from "astro";
+import make_svg from "../../../components/og_image";
+import { getEntryBySlug } from "astro:content";
+import { getBlogPosts, getSnippets, getWeeklies } from "src/content/config";
+
+// Thanks to Arne Bahlo for the inspriation!
+// https://arne.me/articles/static-og-images-in-astro
+
+export async function getStaticPaths() {
+  const blogPaths = (await getBlogPosts()).map((post) => ({
+    params: {
+      slug: post.slug,
+      collection: "blog",
+    },
+  }));
+  const tilPaths = (await getSnippets()).map((post) => ({
+    params: {
+      slug: post.slug,
+      collection: "til",
+    },
+  }));
+  const weeklyPaths = (await getWeeklies()).map((post) => ({
+    params: {
+      slug: post.slug,
+      collection: "weekly",
+    },
+  }));
+  return [...blogPaths, ...tilPaths, ...weeklyPaths];
+}
+
+export const GET: APIRoute = async function get({ params, request }) {
+  let { slug = "", collection } = params;
+  const post = await getEntryBySlug(
+    (collection === "til" ? "snippets" : collection) as
+      | "blog"
+      | "snippets"
+      | "weekly",
+    slug
+  );
+  if (!post) {
+    return new Response(null, { status: 404 });
+  }
+  let root = post.collection === "snippets" ? "til" : post.collection;
+  const path = `${root}/${post.id}`;
+  const svg = await make_svg(
+    post.collection === "blog"
+      ? {
+          title: post.data.title,
+          subtitle: post.data.description || null,
+          date: post.data.date,
+          path,
+          tags: post.data.tags,
+        }
+      : post.collection === "weekly"
+        ? {
+            title: post.data.title,
+            subtitle: null,
+            date: post.data.date,
+            path,
+          }
+        : post.collection === "snippets"
+          ? {
+              title: post.data.description,
+              subtitle: post.data.title,
+              date: post.data.modified || post.data.published,
+              path,
+            }
+          : ({} as never)
+  );
+
+  const png = await sharp(Buffer.from(svg)).png().toBuffer();
+
+  return new Response(png, {
+    headers: {
+      "Content-Type": "image/png",
+    },
+  });
+};
