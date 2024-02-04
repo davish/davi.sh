493f47eba07083cd41be3bfec5ef28e2f74188ea
Davis Haupt <davis@davishaupt.com>
2024-02-04 12:09:20 -0500

    add og-image (#25)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import remarkSidenotes from "@tufte-markdown/remark-sidenotes";
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,5 @@ export default defineConfig({
       theme: "solarized-light",
     },
   },
-  integrations: [],
+  integrations: [react()],
 });
